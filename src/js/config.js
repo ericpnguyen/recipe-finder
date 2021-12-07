ed143ece20b0e525da3683a3aeb73810682199ae
ericnguyen11 <enguyen@hmc.edu>
2021-12-07 15:45:06 -0500

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '3de38bb4-ac4b-4d85-a903-9aaf24471113';
+export const MODAL_CLOSE_SEC = 2.5;

ea38a8d2859de107bb2e7bc6f73e29fe2bb5aec7
ericnguyen11 <enguyen@hmc.edu>
2021-12-08 01:33:50 -0500

    Deleted testing console logs

--- src/js/controller.js
+++ src/js/controller.js
@@ -95,7 +95,6 @@ const controlAddRecipe = async function (newRecipe) {
 
     // Upload the new recipe data
     await model.uploadRecipe(newRecipe);
-    console.log(model.state.recipe);
 
     // Render recipe
     recipeView.render(model.state.recipe);
@@ -127,6 +126,5 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Testing continuous integration');
 };
 init();
